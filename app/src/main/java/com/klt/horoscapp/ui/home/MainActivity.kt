d495dc356e46cf980311cbfb8ba929dd1df90414
KLT <kari9480@gmail.com>
2024-10-14 15:20:17 -0600

    Initial commit

---
+++ app/src/main/java/com/klt/horoscapp/ui/home/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.klt.horoscapp.ui.home
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.klt.horoscapp.R
+import com.klt.horoscapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            val newPaddingTop = systemBars.top
+            val newPaddingBottom = 20
+
+            v.setPadding(systemBars.left, newPaddingTop, systemBars.right, newPaddingBottom)
+            insets
+        }
+
+    }
+}
