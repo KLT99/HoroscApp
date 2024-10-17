4a50cb12ce38ed9ec3884ccd9818f4dd472023c4
KLT <kari9480@gmail.com>
2024-10-17 14:23:01 -0600

    Se añadio linea private val horoscopeViewModel by viewModels<HoroscopeViewModel>() para comunicar el fragment con el viewModel, además se agrego corountine para que se muestre la lista, a su vez se añadio el HoroscopeAdapter para que llenar información y mostrar en el fragment

--- app/src/main/java/com/klt/horoscapp/ui/horoscope/HoroscopeFragment.kt
+++ app/src/main/java/com/klt/horoscapp/ui/horoscope/HoroscopeFragment.kt
@@ -1,16 +1,66 @@
 package com.klt.horoscapp.ui.horoscope
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.klt.horoscapp.databinding.FragmentHoroscopeBinding
+import com.klt.horoscapp.ui.horoscope.Adapter.HoroscopeAdapter
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class HoroscopeFragment : Fragment() {
+
+    private val horoscopeViewModel by viewModels<HoroscopeViewModel>()
+
+    private lateinit var horoscopeAdapter: HoroscopeAdapter
 
     private var _binding: FragmentHoroscopeBinding? = null
     private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initUI()
+    }
+
+    private fun initUI() {
+        initList()
+        initUINState()
+    }
+
+    private fun initList() {
+        horoscopeAdapter = HoroscopeAdapter()
+
+        binding.rvHoroscope.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = horoscopeAdapter
+        }
+        //Segunda forma
+        //binding.rvHoroscope.layoutManager = LinearLayoutManager(context)
+        //binding.rvHoroscope.adapter = horoscopeAdapter
+    }
+
+    private fun initUINState() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                horoscopeViewModel.horoscope.collect{
+                    Log.i("Listado", it.toString())
+
+                    //Cambios en horoscope
+                    horoscopeAdapter.updateList(it)
+                }
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
