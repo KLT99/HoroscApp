b37981297a3493038a496820bbafd4f97d592b9d
KLT <kari9480@gmail.com>
2024-10-17 14:15:22 -0600

    Se creo ViewModel a Horoscope para que gestione los cambios realizados y se muestre en la vista

---
+++ app/src/main/java/com/klt/horoscapp/ui/horoscope/HoroscopeViewModel.kt
@@ -0,0 +1,23 @@
+package com.klt.horoscapp.ui.horoscope
+
+import androidx.lifecycle.ViewModel
+import com.klt.horoscapp.domain.model.HoroscopeInfo
+import com.klt.horoscapp.domain.model.HoroscopeInfo.*
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class HoroscopeViewModel @Inject constructor() : ViewModel() {
+
+    private var _horoscope = MutableStateFlow<List<HoroscopeInfo>>(emptyList())
+    val horoscope: StateFlow<List<HoroscopeInfo>> = _horoscope
+
+    init {
+        _horoscope.value = listOf(
+            Aries, Taurus, Gemini, Cancer
+        )
+    }
+
+}
