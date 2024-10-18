6e8cf1d02cfa668943bdbe343c0709414d02076f
KLT <kari9480@gmail.com>
2024-10-18 12:16:20 -0600

    Se obtuvo los datos del HoroscopeProvider y paso a la lista _horocope en el viewModel

--- app/src/main/java/com/klt/horoscapp/ui/horoscope/HoroscopeViewModel.kt
+++ app/src/main/java/com/klt/horoscapp/ui/horoscope/HoroscopeViewModel.kt
@@ -1,23 +1,20 @@
 package com.klt.horoscapp.ui.horoscope
 
 import androidx.lifecycle.ViewModel
+import com.klt.horoscapp.data.providers.HoroscopeProvider
 import com.klt.horoscapp.domain.model.HoroscopeInfo
-import com.klt.horoscapp.domain.model.HoroscopeInfo.*
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
-class HoroscopeViewModel @Inject constructor() : ViewModel() {
+class HoroscopeViewModel @Inject constructor(horoscopeProvider: HoroscopeProvider) : ViewModel() {
 
     private var _horoscope = MutableStateFlow<List<HoroscopeInfo>>(emptyList())
     val horoscope: StateFlow<List<HoroscopeInfo>> = _horoscope
 
     init {
-        _horoscope.value = listOf(
-            Aries, Taurus, Gemini, Cancer, Leo, Virgo, Libra, Scorpio, Sagittarius, Capricorn, Aquarius, Pisces
-        )
+        _horoscope.value = horoscopeProvider.getHoroscope()
     }
-
 }
