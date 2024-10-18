19bf38e8c9b6509336f48d6075ceb949a3fa777a
KLT <kari9480@gmail.com>
2024-10-18 12:20:00 -0600

    El HoroscopeFragment.kt muestra mediante un toast la información obtenida del itemSelected que se añadio en el HoroscopeAdapter

--- app/src/main/java/com/klt/horoscapp/ui/horoscope/HoroscopeFragment.kt
+++ app/src/main/java/com/klt/horoscapp/ui/horoscope/HoroscopeFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -39,10 +40,12 @@ class HoroscopeFragment : Fragment() {
     }
 
     private fun initList() {
-        horoscopeAdapter = HoroscopeAdapter()
+        horoscopeAdapter = HoroscopeAdapter(onItemSelected = {
+            Toast.makeText(context, getString(it.name), Toast.LENGTH_SHORT).show()
+        })
 
         binding.rvHoroscope.apply {
-            layoutManager = GridLayoutManager(context,2)
+            layoutManager = GridLayoutManager(context, 2)
             adapter = horoscopeAdapter
         }
         //Segunda forma
@@ -52,8 +55,8 @@ class HoroscopeFragment : Fragment() {
 
     private fun initUINState() {
         lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED){
-                horoscopeViewModel.horoscope.collect{
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                horoscopeViewModel.horoscope.collect {
                     Log.i("Listado", it.toString())
 
                     //Cambios en horoscope, asigna el listado al Adapter para mostrar en el fragment
