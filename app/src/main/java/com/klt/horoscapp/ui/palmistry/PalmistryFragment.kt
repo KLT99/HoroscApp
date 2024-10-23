30c5200bc7e562f6558bfea1065fdd2d32281b89
KLT <kari9480@gmail.com>
2024-10-23 10:32:46 -0600

    se modifico clase PalmistryFragment.kt

--- app/src/main/java/com/klt/horoscapp/ui/palmistry/PalmistryFragment.kt
+++ app/src/main/java/com/klt/horoscapp/ui/palmistry/PalmistryFragment.kt
@@ -17,7 +17,7 @@ class PalmistryFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentPalmistryBinding.inflate(layoutInflater, container, false)
         // Inflate the layout for this fragment
         return binding.root
