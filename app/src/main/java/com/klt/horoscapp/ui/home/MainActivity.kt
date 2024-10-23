69440899f81f2f4ca3e0e456637f298ad00c8684
KLT <kari9480@gmail.com>
2024-10-23 10:38:08 -0600

    se agrego funcion initNavigation para navegar entre los fragments de la activity

--- app/src/main/java/com/klt/horoscapp/ui/home/MainActivity.kt
+++ app/src/main/java/com/klt/horoscapp/ui/home/MainActivity.kt
@@ -5,6 +5,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.klt.horoscapp.R
 import com.klt.horoscapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,6 +16,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +33,16 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, newPaddingTop, systemBars.right, newPaddingBottom)
             insets
         }
+        initUI()
+    }
 
+    private fun initUI() {
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomNavView.setupWithNavController(navController)
     }
 }
